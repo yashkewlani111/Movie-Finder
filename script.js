402d4a35db5e2130a0213974c0686f1e0459a279
yashkewlani111 <56068115+yashkewlani111@users.noreply.github.com>
2020-10-12 21:17:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const APIURL =
-    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+    "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key={apikey}&page=1";      //paste apikey here
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI =
-    "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+    "https://api.themoviedb.org/3/search/movie?&api_key={apikey}&query=";   //paste apikey here
 
 const main = document.getElementById("main");
 const form = document.getElementById("form");
